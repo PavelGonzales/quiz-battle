9f11b2d12abe5415a08af2abc37cdda468f674f2
Pavel Gonzales <develop.gonzales@gmail.com>
2020-07-26 20:56:34 +0300

    Add mock

---
+++ src/mocks/quiz.js
@@ -0,0 +1,74 @@
+export default {
+  "response_code": 0,
+  "results": [{
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which game did &quot;Sonic The Hedgehog&quot; make his first appearance in?",
+    "correct_answer": "Rad Mobile",
+    "incorrect_answers": ["Sonic The Hedgehog", "Super Mario 64", "Mega Man"]
+  }, {
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "What ingredient is NOT used to craft a cake in Minecraft?",
+    "correct_answer": "Bread",
+    "incorrect_answers": ["Wheat", "Milk", "Egg"]
+  }, {
+    "category": "Animals",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "How many legs do butterflies have?",
+    "correct_answer": "6",
+    "incorrect_answers": ["2", "4", "0"]
+  }, {
+    "category": "General Knowledge",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "What is the French word for &quot;hat&quot;?",
+    "correct_answer": "Chapeau",
+    "incorrect_answers": ["Bonnet", " &Eacute;charpe", " Casque"]
+  }, {
+    "category": "Science & Nature",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which is the most abundant element in the universe?",
+    "correct_answer": "Hydrogen",
+    "incorrect_answers": ["Helium", "Lithium", "Oxygen"]
+  }, {
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which of the following is NOT a summon in the game &quot;South Park: The Stick of Truth&quot;?",
+    "correct_answer": "Towelie",
+    "incorrect_answers": ["Jesus", "Mr. Hankey", "Mr. Slave"]
+  }, {
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "In what year was the first &quot;Mass Effect&quot; game released?",
+    "correct_answer": "2007",
+    "incorrect_answers": ["2008", "2009", "2010"]
+  }, {
+    "category": "Entertainment: Film",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "When does &quot;Rogue One: A Star Wars Story&quot; take place chronologically in the series?",
+    "correct_answer": "Between Episode 3 and 4",
+    "incorrect_answers": ["After Episode 6", "Before Episode 1", "Between Episode 4 and 5"]
+  }, {
+    "category": "Entertainment: Film",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "In the movie &quot;Blade Runner&quot;, what is the term used for human-like androids ?",
+    "correct_answer": "Replicants",
+    "incorrect_answers": ["Cylons", "Synthetics", "Skinjobs"]
+  }, {
+    "category": "Entertainment: Japanese Anime & Manga",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Satella in &quot;Re:Zero&quot; is the witch of what?",
+    "correct_answer": "Envy",
+    "incorrect_answers": ["Pride", "Sloth", "Wrath"]
+  }]
+}
