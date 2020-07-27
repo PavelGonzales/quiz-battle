e807b7736600399923beca7f640abf004f5a9f8c
Pavel Gonzales <develop.gonzales@gmail.com>
2020-07-27 11:13:08 +0300

    Base layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Space, Typography, Avatar } from 'antd';
+import { Button, Space, Typography, Avatar, Progress } from 'antd';
+import { ClockCircleOutlined } from '@ant-design/icons';
 import './App.css';
 import mock from './mocks/quiz';
 
@@ -7,45 +8,59 @@ console.log('mock', mock)
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [rightQuestions, setRightQuestions] = useState(0);
+  const [timer, setTimer] = useState(30);
 
   return (
     <div className="App">
-      <Space
-        direction="vertical"
-        align="center"
-        style={{ width: '100%' }}
-      >
+      <div className="timerContainer">
+        <ClockCircleOutlined className="timerContainer__icon" />
+        { timer }
+      </div>
+      <Progress
+        percent={100}
+        success={{ percent: 50 }}
+        showInfo={false}
+        status="exception"
+        className="progress"
+      />
+      <div className="userContainer">
         <Avatar
           src={`https://api.adorable.io/avatars/100/${Math.random()}`}
           size={100}
         />
-        <div>{ currentQuestion } / 10</div>
         <div className="rightQustions">{ rightQuestions }</div>
+      </div>
 
-      </Space>
-      
-      <Typography.Title level={4}>
-        <div dangerouslySetInnerHTML={{ __html: mock.results[currentQuestion].question }} />
-      </Typography.Title>
-      
-      <Space
-        direction="vertical"
-        style={{ width: '100%', marginTop: 'auto' }}
-      >
-        {
-          [
-            ...mock.results[currentQuestion].incorrect_answers, 
-            mock.results[currentQuestion].correct_answer
-          ].map(answer => <Button 
-            key={answer}
-            block
-            type="primary" 
-            onClick={() => {setCurrentQuestion(currentQuestion + 1); setRightQuestions(currentQuestion + 1) }}
-          >
-            {answer}
-          </Button>)
-        }
-      </Space>
+      {
+        currentQuestion < 10 ? (
+          <>
+            <Typography.Title level={4}>
+              <div dangerouslySetInnerHTML={{ __html: mock.results[currentQuestion].question }} />
+            </Typography.Title>
+            
+            <Space
+              direction="vertical"
+              style={{ width: '100%', marginTop: 'auto' }}
+            >
+              {
+                [
+                  ...mock.results[currentQuestion].incorrect_answers, 
+                  mock.results[currentQuestion].correct_answer
+                ].map(answer => <Button 
+                  key={answer}
+                  block
+                  type="primary" 
+                  shape="round"
+                  size="large"
+                  onClick={() => {setCurrentQuestion(currentQuestion + 1); setRightQuestions(currentQuestion + 1) }}
+                >
+                  {answer}
+                </Button>)
+              }
+            </Space>
+          </>
+        ) : 'The End'
+      }
     </div>
   );
 }
